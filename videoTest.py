75068eaeb580d793072a6fb6e6d853ad38c958be
kellyycha <65841983+kellyycha@users.noreply.github.com>
2020-11-07 11:21:01 -0500

    Update videoTest.py

--- videoTest.py
+++ videoTest.py
@@ -5,9 +5,10 @@ import dlib
 cap = cv2.VideoCapture(0)
 
 detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 while True:
-    success, frame = cap.read()
+    _, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = detector(gray)
@@ -16,10 +17,18 @@ while True:
         y1 = face.top()
         x2 = face.right()
         y2 = face.bottom()
+        #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
 
-        cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 3)
+        landmarks = predictor(gray, face)
+
+        for n in range(0, 68):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+            cv2.circle(frame, (x, y), 4, (255, 0, 0), -1)
 
 
-    cv2.imshow("Camera", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
         break
